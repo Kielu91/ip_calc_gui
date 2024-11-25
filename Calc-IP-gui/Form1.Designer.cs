335528e7efc679c56b3b917be473be85fe7b825a
kielu91 <lukaszkielczyk@gmail.com>
2024-11-25 21:33:14 +0100

    Initial commit

---
+++ Calc-IP-gui/Form1.Designer.cs
@@ -0,0 +1,50 @@
+﻿namespace Calc_IP_gui;
+
+partial class Form1
+{
+    /// <summary>
+    ///  Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    ///  Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+        if (disposing && (components != null))
+        {
+            components.Dispose();
+        }
+
+        base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+        SuspendLayout();
+        // 
+        // Form1
+        // 
+        AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+        AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+        BackColor = System.Drawing.Color.MediumTurquoise;
+        ClientSize = new System.Drawing.Size(784, 561);
+        Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)238));
+        ForeColor = System.Drawing.SystemColors.ControlText;
+        Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+        MaximizeBox = false;
+        StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+        Text = "IP calculator";
+        ResumeLayout(false);
+    }
+
+    #endregion
+}
---
+++ Calc-IP-gui/Form1.cs
@@ -0,0 +1,9 @@
+namespace Calc_IP_gui;
+
+public partial class Form1 : Form
+{
+    public Form1()
+    {
+        InitializeComponent();
+    }
+}
