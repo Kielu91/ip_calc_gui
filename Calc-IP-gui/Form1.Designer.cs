cd57988e63b5815be22cedeb9de322c340825dde
Kielu91 <lukaszkielczyk@gmail.com>
2024-12-06 14:22:05 +0100

    +blueprint gui (to rebuild)

--- Calc-IP-gui/Form1.Designer.cs
+++ Calc-IP-gui/Form1.Designer.cs
@@ -29,22 +29,373 @@ partial class Form1
     /// </summary>
     private void InitializeComponent()
     {
+        label1 = new System.Windows.Forms.Label();
+        label2 = new System.Windows.Forms.Label();
+        label3 = new System.Windows.Forms.Label();
+        textBox1 = new System.Windows.Forms.TextBox();
+        textBox2 = new System.Windows.Forms.TextBox();
+        label4 = new System.Windows.Forms.Label();
+        label5 = new System.Windows.Forms.Label();
+        label6 = new System.Windows.Forms.Label();
+        label7 = new System.Windows.Forms.Label();
+        label8 = new System.Windows.Forms.Label();
+        label9 = new System.Windows.Forms.Label();
+        label10 = new System.Windows.Forms.Label();
+        label11 = new System.Windows.Forms.Label();
+        label12 = new System.Windows.Forms.Label();
+        label13 = new System.Windows.Forms.Label();
+        label14 = new System.Windows.Forms.Label();
+        label15 = new System.Windows.Forms.Label();
+        label16 = new System.Windows.Forms.Label();
+        label17 = new System.Windows.Forms.Label();
+        label18 = new System.Windows.Forms.Label();
+        label19 = new System.Windows.Forms.Label();
+        label20 = new System.Windows.Forms.Label();
+        label21 = new System.Windows.Forms.Label();
+        label22 = new System.Windows.Forms.Label();
+        label23 = new System.Windows.Forms.Label();
+        label24 = new System.Windows.Forms.Label();
         SuspendLayout();
+        // 
+        // label1
+        // 
+        label1.BackColor = System.Drawing.Color.Wheat;
+        label1.Font = new System.Drawing.Font("Consolas", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)238));
+        label1.ForeColor = System.Drawing.Color.Crimson;
+        label1.Location = new System.Drawing.Point(366, 6);
+        label1.Name = "label1";
+        label1.Size = new System.Drawing.Size(404, 83);
+        label1.TabIndex = 0;
+        label1.Text = "IP Calculator";
+        label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+        // 
+        // label2
+        // 
+        label2.BackColor = System.Drawing.Color.Blue;
+        label2.Location = new System.Drawing.Point(15, 15);
+        label2.Name = "label2";
+        label2.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+        label2.Size = new System.Drawing.Size(300, 50);
+        label2.TabIndex = 1;
+        label2.Text = "IP adres";
+        label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+        // 
+        // label3
+        // 
+        label3.BackColor = System.Drawing.Color.Blue;
+        label3.Location = new System.Drawing.Point(15, 66);
+        label3.Name = "label3";
+        label3.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+        label3.Size = new System.Drawing.Size(300, 50);
+        label3.TabIndex = 2;
+        label3.Text = "mask";
+        label3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+        // 
+        // textBox1
+        // 
+        textBox1.Location = new System.Drawing.Point(123, 25);
+        textBox1.Name = "textBox1";
+        textBox1.Size = new System.Drawing.Size(186, 31);
+        textBox1.TabIndex = 3;
+        // 
+        // textBox2
+        // 
+        textBox2.Location = new System.Drawing.Point(123, 76);
+        textBox2.Name = "textBox2";
+        textBox2.Size = new System.Drawing.Size(186, 31);
+        textBox2.TabIndex = 4;
+        // 
+        // label4
+        // 
+        label4.BackColor = System.Drawing.Color.MintCream;
+        label4.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)238));
+        label4.Location = new System.Drawing.Point(15, 163);
+        label4.Name = "label4";
+        label4.Size = new System.Drawing.Size(150, 45);
+        label4.TabIndex = 5;
+        label4.Text = "Ip adres";
+        label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+        // 
+        // label5
+        // 
+        label5.BackColor = System.Drawing.Color.MintCream;
+        label5.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)238));
+        label5.Location = new System.Drawing.Point(15, 209);
+        label5.Name = "label5";
+        label5.Size = new System.Drawing.Size(150, 45);
+        label5.TabIndex = 6;
+        label5.Text = "mask";
+        label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+        // 
+        // label6
+        // 
+        label6.BackColor = System.Drawing.Color.MintCream;
+        label6.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)238));
+        label6.Location = new System.Drawing.Point(15, 255);
+        label6.Name = "label6";
+        label6.Size = new System.Drawing.Size(150, 45);
+        label6.TabIndex = 7;
+        label6.Text = "adres sieci";
+        label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+        // 
+        // label7
+        // 
+        label7.BackColor = System.Drawing.Color.MintCream;
+        label7.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)238));
+        label7.Location = new System.Drawing.Point(15, 301);
+        label7.Name = "label7";
+        label7.Size = new System.Drawing.Size(150, 45);
+        label7.TabIndex = 8;
+        label7.Text = "adres rozgloszeniowy";
+        label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+        // 
+        // label8
+        // 
+        label8.BackColor = System.Drawing.Color.MintCream;
+        label8.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)238));
+        label8.Location = new System.Drawing.Point(15, 347);
+        label8.Name = "label8";
+        label8.Size = new System.Drawing.Size(150, 45);
+        label8.TabIndex = 9;
+        label8.Text = "hostow w sieci";
+        label8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+        // 
+        // label9
+        // 
+        label9.BackColor = System.Drawing.Color.MintCream;
+        label9.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)238));
+        label9.Location = new System.Drawing.Point(15, 393);
+        label9.Name = "label9";
+        label9.Size = new System.Drawing.Size(150, 45);
+        label9.TabIndex = 10;
+        label9.Text = "host min";
+        label9.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+        // 
+        // label10
+        // 
+        label10.BackColor = System.Drawing.Color.MintCream;
+        label10.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)238));
+        label10.Location = new System.Drawing.Point(15, 439);
+        label10.Name = "label10";
+        label10.Size = new System.Drawing.Size(150, 45);
+        label10.TabIndex = 11;
+        label10.Text = "host max";
+        label10.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+        // 
+        // label11
+        // 
+        label11.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)238));
+        label11.Location = new System.Drawing.Point(168, 163);
+        label11.Name = "label11";
+        label11.Size = new System.Drawing.Size(160, 45);
+        label11.TabIndex = 12;
+        label11.Text = "148.181.137.221";
+        label11.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+        // 
+        // label12
+        // 
+        label12.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)238));
+        label12.Location = new System.Drawing.Point(168, 209);
+        label12.Name = "label12";
+        label12.Size = new System.Drawing.Size(160, 45);
+        label12.TabIndex = 13;
+        label12.Text = "148.81.137.221";
+        label12.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+        // 
+        // label13
+        // 
+        label13.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)238));
+        label13.Location = new System.Drawing.Point(168, 255);
+        label13.Name = "label13";
+        label13.Size = new System.Drawing.Size(160, 45);
+        label13.TabIndex = 14;
+        label13.Text = "148.81.137.221";
+        label13.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+        // 
+        // label14
+        // 
+        label14.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)238));
+        label14.Location = new System.Drawing.Point(168, 301);
+        label14.Name = "label14";
+        label14.Size = new System.Drawing.Size(160, 45);
+        label14.TabIndex = 15;
+        label14.Text = "148.81.137.221";
+        label14.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+        // 
+        // label15
+        // 
+        label15.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)238));
+        label15.Location = new System.Drawing.Point(168, 347);
+        label15.Name = "label15";
+        label15.Size = new System.Drawing.Size(160, 45);
+        label15.TabIndex = 16;
+        label15.Text = "148.81.137.221";
+        label15.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+        // 
+        // label16
+        // 
+        label16.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)238));
+        label16.Location = new System.Drawing.Point(168, 393);
+        label16.Name = "label16";
+        label16.Size = new System.Drawing.Size(160, 45);
+        label16.TabIndex = 17;
+        label16.Text = "148.81.137.221";
+        label16.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+        // 
+        // label17
+        // 
+        label17.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)238));
+        label17.Location = new System.Drawing.Point(168, 439);
+        label17.Name = "label17";
+        label17.Size = new System.Drawing.Size(160, 45);
+        label17.TabIndex = 18;
+        label17.Text = "148.81.137.221";
+        label17.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+        // 
+        // label18
+        // 
+        label18.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)238));
+        label18.Location = new System.Drawing.Point(334, 163);
+        label18.Name = "label18";
+        label18.Size = new System.Drawing.Size(370, 45);
+        label18.TabIndex = 19;
+        label18.Text = "10010100.01010001.1000 1001.11011101\t";
+        label18.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+        // 
+        // label19
+        // 
+        label19.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)238));
+        label19.Location = new System.Drawing.Point(334, 209);
+        label19.Name = "label19";
+        label19.Size = new System.Drawing.Size(370, 45);
+        label19.TabIndex = 20;
+        label19.Text = "10010100.01010001.1000 1001.11011101\t";
+        label19.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+        // 
+        // label20
+        // 
+        label20.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)238));
+        label20.Location = new System.Drawing.Point(334, 255);
+        label20.Name = "label20";
+        label20.Size = new System.Drawing.Size(370, 45);
+        label20.TabIndex = 21;
+        label20.Text = "10010100.01010001.1000 1001.11011101\t";
+        label20.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+        // 
+        // label21
+        // 
+        label21.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)238));
+        label21.Location = new System.Drawing.Point(334, 301);
+        label21.Name = "label21";
+        label21.Size = new System.Drawing.Size(370, 45);
+        label21.TabIndex = 22;
+        label21.Text = "10010100.01010001.1000 1001.11011101\t";
+        label21.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+        // 
+        // label22
+        // 
+        label22.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)238));
+        label22.Location = new System.Drawing.Point(334, 347);
+        label22.Name = "label22";
+        label22.Size = new System.Drawing.Size(370, 45);
+        label22.TabIndex = 23;
+        label22.Text = "10010100.01010001.1000 1001.11011101\t";
+        label22.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+        // 
+        // label23
+        // 
+        label23.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)238));
+        label23.Location = new System.Drawing.Point(334, 393);
+        label23.Name = "label23";
+        label23.Size = new System.Drawing.Size(370, 45);
+        label23.TabIndex = 24;
+        label23.Text = "10010100.01010001.1000 1001.11011101\t";
+        label23.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+        // 
+        // label24
+        // 
+        label24.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)238));
+        label24.Location = new System.Drawing.Point(334, 438);
+        label24.Name = "label24";
+        label24.Size = new System.Drawing.Size(370, 45);
+        label24.TabIndex = 25;
+        label24.Text = "10010100.01010001.1000 1001.11011101\t";
+        label24.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
         // 
         // Form1
         // 
-        AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
-        AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+        AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
         BackColor = System.Drawing.Color.MediumTurquoise;
-        ClientSize = new System.Drawing.Size(784, 561);
-        Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)238));
+        ClientSize = new System.Drawing.Size(778, 544);
+        Controls.Add(label24);
+        Controls.Add(label23);
+        Controls.Add(label22);
+        Controls.Add(label21);
+        Controls.Add(label20);
+        Controls.Add(label19);
+        Controls.Add(label18);
+        Controls.Add(label17);
+        Controls.Add(label16);
+        Controls.Add(label15);
+        Controls.Add(label14);
+        Controls.Add(label13);
+        Controls.Add(label12);
+        Controls.Add(label11);
+        Controls.Add(label10);
+        Controls.Add(label9);
+        Controls.Add(label8);
+        Controls.Add(label7);
+        Controls.Add(label6);
+        Controls.Add(label5);
+        Controls.Add(label4);
+        Controls.Add(textBox2);
+        Controls.Add(textBox1);
+        Controls.Add(label3);
+        Controls.Add(label2);
+        Controls.Add(label1);
+        Font = new System.Drawing.Font("Consolas", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)238));
         ForeColor = System.Drawing.SystemColors.ControlText;
-        Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+        Location = new System.Drawing.Point(22, 22);
+        Margin = new System.Windows.Forms.Padding(4);
         MaximizeBox = false;
-        StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-        Text = "IP calculator";
+        StartPosition = System.Windows.Forms.FormStartPosition.Manual;
         ResumeLayout(false);
+        PerformLayout();
     }
+
+    private System.Windows.Forms.Label label19;
+    private System.Windows.Forms.Label label20;
+    private System.Windows.Forms.Label label21;
+    private System.Windows.Forms.Label label22;
+    private System.Windows.Forms.Label label23;
+    private System.Windows.Forms.Label label24;
+
+    private System.Windows.Forms.Label label12;
+    private System.Windows.Forms.Label label13;
+    private System.Windows.Forms.Label label14;
+    private System.Windows.Forms.Label label15;
+    private System.Windows.Forms.Label label16;
+    private System.Windows.Forms.Label label17;
+    private System.Windows.Forms.Label label18;
+
+    private System.Windows.Forms.Label label11;
+
+    private System.Windows.Forms.Label label9;
+    private System.Windows.Forms.Label label10;
+
+    private System.Windows.Forms.Label label7;
+    private System.Windows.Forms.Label label8;
+
+    private System.Windows.Forms.TextBox textBox1;
+    private System.Windows.Forms.TextBox textBox2;
+    private System.Windows.Forms.Label label4;
+    private System.Windows.Forms.Label label5;
+    private System.Windows.Forms.Label label6;
+
+    private System.Windows.Forms.Label label2;
+    private System.Windows.Forms.Label label3;
+
+    private System.Windows.Forms.Label label1;
 
     #endregion
 }
